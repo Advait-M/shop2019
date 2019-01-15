3ac96fd40ef91ecabf45bc2def8258b33d5d3904
Advait-M <advait.maybhate9@gmail.com>
2019-01-14 22:11:46 -0500

    Switch to ES5 syntax

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-import express from "express";
-import expressGraphQL from "express-graphql";
-import mongoose from "mongoose";
-import bodyParser from "body-parser";
-import cors from "cors";
+express = require("express")
+expressGraphQL = require("express-graphql")
+mongoose = require("mongoose")
+bodyParser = require("body-parser")
+cors = require("cors")
+schema = require("./graphql/")
 
-import schema from "./graphql/";
+// import express from "express";
+// import expressGraphQL from "express-graphql";
+// import mongoose from "mongoose";
+// import bodyParser from "body-parser";
+// import cors from "cors";
+
+// import schema from "./graphql/";
 
 const app = express();
 const PORT = process.env.PORT || "5000";
@@ -46,6 +53,8 @@ const db = "mongodb://admin:shop#123@ds115350.mlab.com:15350/shopdb";
 
 console.log("test deploy")
 
+
+const path = require('path')
 app
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
