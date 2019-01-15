5c4e2cf50f142939b709cca88618162ced70d9da
Advait-M <advait.maybhate9@gmail.com>
2019-01-14 21:36:55 -0500

    Adding basic GraphQL API queries with "users" model for test with Heroku.

--- index.js
+++ index.js
@@ -1,12 +1,45 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+import express from "express";
+import expressGraphQL from "express-graphql";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import cors from "cors";
 
-console.log("test deploy")
+import schema from "./graphql/";
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const app = express();
+const PORT = process.env.PORT || "5000";
+const db = "mongodb://admin:shop#123@ds115350.mlab.com:15350/shopdb";
+
+const allowedMethods = ['GET','POST'];
+
+function onrequest(req, res) {
+  if (!allowedMethods.includes(req.method)) {
+    	return res.end(405, 'Method Not Allowed');
+	}
+}
+
+// Testing GraphQL
+
+mongoose
+  .connect(
+    db,
+    {
+      useCreateIndex: true,
+      useNewUrlParser: true
+    }
+  )
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+console.log("Checkpoint 1")
+app.use(
+  "/graphql",
+  cors(),
+  bodyParser.json(),
+  expressGraphQL({
+    schema,
+    graphiql: false
+  })
+);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
