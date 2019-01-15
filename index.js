39bde5a6061eb5169920433679a37fee634d9188
Advait-M <advait.maybhate9@gmail.com>
2019-01-14 22:08:52 -0500

    Reverting to previous express

--- index.js
+++ index.js
@@ -10,36 +10,45 @@ const app = express();
 const PORT = process.env.PORT || "5000";
 const db = "mongodb://admin:shop#123@ds115350.mlab.com:15350/shopdb";
 
-const allowedMethods = ['GET','POST'];
+// const allowedMethods = ['GET','POST'];
 
-function onrequest(req, res) {
-  if (!allowedMethods.includes(req.method)) {
-    	return res.end(405, 'Method Not Allowed');
-	}
-}
+// function onrequest(req, res) {
+//   if (!allowedMethods.includes(req.method)) {
+//     	return res.end(405, 'Method Not Allowed');
+// 	}
+// }
 
-// Testing GraphQL
+// // Testing GraphQL
 
-mongoose
-  .connect(
-    db,
-    {
-      useCreateIndex: true,
-      useNewUrlParser: true
-    }
-  )
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.log(err));
+// mongoose
+//   .connect(
+//     db,
+//     {
+//       useCreateIndex: true,
+//       useNewUrlParser: true
+//     }
+//   )
+//   .then(() => console.log("MongoDB connected"))
+//   .catch(err => console.log(err));
 
-console.log("Checkpoint 1")
-app.use(
-  "/graphql",
-  cors(),
-  bodyParser.json(),
-  expressGraphQL({
-    schema,
-    graphiql: false
-  })
-);
+// console.log("Checkpoint 1")
+// app.use(
+//   "/graphql",
+//   cors(),
+//   bodyParser.json(),
+//   expressGraphQL({
+//     schema,
+//     graphiql: false
+//   })
+// );
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+console.log("test deploy")
+
+app
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
